5d60ac13d8f7999b846a1053645ea2a98ab03268
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-10-20 22:45:27 -0300

    Tela de configuração do cronometro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import SetPomodoro from "./components/SetPomodoro";
 
 function App() {
   return (
     <div className="container">
       <h1>Pomodoro </h1>
       <small>Seja Produtivo da forma correta</small>
-
+      <SetPomodoro/>
     </div>
   );
 }
